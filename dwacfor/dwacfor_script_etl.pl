7e188b37f385d92929652bba5c465c7c7c4ff0a6
edilson <edilson@edilson-sefin.(none)>
2012-10-17 16:19:31 -0300

    ajuste script

--- dwacfor/dwacfor_script_etl.pl
+++ dwacfor/dwacfor_script_etl.pl
@@ -19,15 +19,29 @@ my @files_list;
 opendir(DIR, $dir) or die $!;
 
 # apagando os dados da tabela
-backup_table("ods_valores_variaveis");
+####backup_table("fato_indicador");
 
 # lendo diretórios
 while (my $file = readdir(DIR)) {
-	print "Lendo $file ........................................ \n";
+	##print "Lendo $file ........................................ \n";
 	next if ($file =~ m/^\./);  # Use a regular expression to ignore files beginning with a period
 	ler_arquivo($dir, $file);
-##	print "Arquivo $file foi salvo.............................\n";
+	##print "Arquivo $file foi salvo.............................\n";
 }
+
+
+#INSERT INTO fato_indicador(
+#	id_dim_tempo, 
+#	id_dim_municipio, 
+#	id_dim_unidade_administrativa, 
+#	id_dim_indicador, 
+#	valor_indicador) 
+#(select t.id, 
+#	(select m.id from dim_municipio m where m.codigo = 9869), 
+#	(select u.id from dim_unidade_administrativa u where u.codigo = 48) , 
+#	(select v.id from dim_variavel v where v.codigo = 3641) , 3550 
+#from dim_tempo t where t.ano_mes = 201112)
+
 
 # lendo arquivo
 sub ler_arquivo {
@@ -42,45 +56,73 @@ sub ler_arquivo {
 		if($cont > 1){
 			my($setor, $ano, $mes, $local, $unid, $cdvar, $vlvar) = split(";");
 			$vlvar =~ s/,/./;
-			my $insert = "INSERT INTO ods_valores_variaveis(setor, ano_referencia, mes_referencia, localidade, unidade, codigo_variavel, valor_variavel, numero_linha_arquivo) VALUES($setor, $ano, $mes, $local, $unid, $cdvar, $vlvar, $cont+1);";
-			salvar_registro("$insert") or die "Erro ao tentar inserir registro ...";
+			my $insert = "INSERT INTO fato_indicador(
+				id_dim_tempo, 
+				id_dim_municipio, 
+				id_dim_unidade_administrativa, 
+				id_dim_indicador, 
+				valor_indicador) 
+			select id, $local, $unid, $cdvar, $vlvar from dim_tempo where ano_mes = $ano$mes;";
+			print "$insert \n";
+			
+			#### salvar_registro("$insert") or die "Erro ao tentar inserir registro ...";
 		}
 		$cont++;
 	}
 	close(FILE);
 }
 
+#INSERT INTO fato_indicador(id_dim_tempo, id_dim_municipio, id_dim_unidade_administrativa, 
+#id_dim_indicador, valor_indicador) select id, 1, 84, 11, 2323.8 from dim_tempo where ano_mes = 201010;
+
+
 # salvando registro
 sub salvar_registro{
 	my ($row) = @_;
-	print "Salvando registro [$row ]...\n";
+	## print "Salvando registro [$row ]...\n";
 	my $query = $dbi->prepare($row) || die $dbi->errstr;
 	$query->execute() or die "Não foi possível inserir registro ...";
 	$query->finish();
-	print "Salvou ....\n";
+	## print "Salvou ....\n";
 }
 
 closedir(DIR);
+
+
+# id_dim_tempo bigint,
+# id_dim_municipio bigint,
+# id_dim_unidade_administrativa bigint,
+# id_dim_indicador bigint,
+# valor_indicador numeric(16,2),
 
 # limpa tabelas
 sub backup_table{
 
     my ($tb) = @_;
-    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime time;
-    
-    my $sth_queues = $dbi->prepare("ALTER TABLE ods_valores_variaveis RENAME TO ods_valores_variaveis$mon$mday$hour$min$sec;");
-    $sth_queues->execute() or die "Não foi possível renomear tabelas ...";
-    print "Tabela  [ods_valores_variaveis] renomeada!\n";
+    my ($sec,$min,$hour,$day,$mon,$year,$wday,$yday,$isdst) = localtime time;
+    $mon = $mon+1;
+
+    my $dat = "$year$mon$day";
+    my $hor = "$hour$min";
+    my $suf = "$dat" . "_" . "$hor";
+
+    my $sth_queues = $dbi->prepare("ALTER TABLE fato_indicador RENAME TO fato_indicador_$suf;");
+    $sth_queues->execute() or die "Não foi possível renomear tabela ...";
+    print "Tabela  [fato_indicador] renomeada!\n";
 	
 	## TODO: VERIFICAR SE A TABALA EXISTE. CASO A MESMA NÃO EXISTE VAI GERAR ERRO
 	 
-   $sth_queues = $dbi->prepare("CREATE TABLE ods_valores_variaveis(setor character varying(2) NOT NULL, 
-	ano_referencia integer NOT NULL, mes_referencia integer NOT NULL, localidade integer NOT NULL, unidade integer NOT NULL, 
-	codigo_variavel integer NOT NULL, valor_variavel character varying(100), numero_linha_arquivo integer, 
-	CONSTRAINT pk_ods_vars_$mon$mday$hour$min$sec PRIMARY KEY (setor , ano_referencia , mes_referencia , localidade , unidade, codigo_variavel ))") or die $dbi->errstr;
+   $sth_queues = $dbi->prepare("CREATE TABLE fato_indicador(
+		id bigserial NOT NULL,
+		id_dim_tempo bigint,
+		id_dim_municipio bigint, 
+		id_dim_unidade_administrativa bigint, 
+		id_dim_indicador bigint , 
+		valor_indicador numeric(16,2),
+	   CONSTRAINT pk_fato_indicador_$suf PRIMARY KEY (id))") or die $dbi->errstr;
     
    $sth_queues->execute() or die "Não foi possível criar tabela ...";
-   print "Tabela  [ods_valores_variaveis] criada!\n";
+   print "Tabela  [fato_indicador] criada!\n";
 }
 
 
--- dwacfor/dwacfor_update.sh
+++ dwacfor/dwacfor_update.sh
@@ -8,20 +8,17 @@
 
 
 echo "CONECTANDO EM ftp://ftp.cagece.com.br ....... "
-sudo wget ftp://ftp.cagece.com.br/*.txt --user='ACFOR' --password='A@CFO$R' -P /opt/dwacfor/ftp_files/ 2> /tmp/dwacfor_ftp.err &
+wget ftp://ftp.cagece.com.br/*.txt --user='ACFOR' --password='A@CFO$R' -P /opt/dwacfor/ftp_files/ 2> /tmp/dwacfor_ftp.err
 
 echo "ARQUIVOS DE ftp://ftp.cagece.com.br BAIXADOS ..."
 
-sleep 10
-
 echo "EXECUTANDO ETL ...."
-sudo perl /opt/dwacfor/dwacfor_script_etl.pl 2> /tmp/dwacfor_etl.err &
+sudo perl /opt/dwacfor/dwacfor_script_etl.pl 2> /tmp/dwacfor_etl.err
 
 echo "ETL EXECUTADO COM SUCESSO !!!"
 
-sleep 5
-exec sudo rm -rf /opt/dwacfor/ftp_files/*
+rm -rf /opt/dwacfor/ftp_files/*
 echo "ARQUIVOS BAIXADOS APAGADOS!"
 
-sleep 3
+sleep 1
 exit 0
